0bf3cd73906d0d243754d9b61f4f6893e465c098
heesun <heesun9793@naver.com>
2023-03-27 17:53:58 +0900

    연습중

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart2/components/shoppingcart_detail.dart';
+import 'package:flutter_shoppingcart2/components/shoppingcart_header.dart';
+import 'package:flutter_shoppingcart2/constants.dart';
+import 'package:flutter_shoppingcart2/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: kPrimaryColor,
+      debugShowCheckedModeBanner: false,
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+}
+
+AppBar _buildShoppingCartAppBar() {
+  return AppBar(
+    foregroundColor: Colors.black,
+    backgroundColor: kPrimaryColor,
+    leading: IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {},
+    ),
+    actions: [
+      IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {},),
+      SizedBox(width: 10),
+    ],
+    elevation: 0.0,
+  );
+}
---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart2/constants.dart';
+
+AppBarTheme theme() {
+  return AppBarTheme(
+    backgroundColor: kPrimaryColor,
+    foregroundColor: kPrimaryColor,
+  );
+}
